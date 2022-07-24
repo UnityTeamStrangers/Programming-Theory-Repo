3babf283cdabb0bc07dd102a4c3dc9796deb6688
UnityTeamStrangers <GCTeamStrangers@gmail.com>
2022-07-24 14:33:06 +1000

    Изменены параметры при нажатии на "+"

--- Prototype_6/Assets/Scripts/MyClass.cs
+++ Prototype_6/Assets/Scripts/MyClass.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 
 /// <summary>
 /// Производный класс
 /// </summary>
 public class MyClass : MainClass
-{
+{   
     public void HP_Plus()
     {
         if (point != 0)
@@ -15,8 +18,8 @@ public class MyClass : MainClass
             point--;
         }
     }
-    public override void DMG_Plus() { Damage++; base.DMG_Plus(); }
-    public override void END_Plus() { if (point != 0) { Endurance+=2; point--; } }
+    public override void DMG_Plus() { Damage+=10; base.DMG_Plus(); }
+    public override void END_Plus() { if (point != 0) { Endurance+=10; point--; } }
     public override void SetParams(int hp, int dmg, int end)
     {
         Set_HP_Max(hp);
@@ -29,6 +32,6 @@ public class MyClass : MainClass
     void Awake()
     {
         SetNameClass("Player_2");
-        SetParams(200, 20, 20);
+        SetParams(400, 0, 40);
     }
 }
